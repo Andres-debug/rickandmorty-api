c484b237e5828230f6209fdc9dcd4e5f51c14e1f
Andres Felipe Diaz <andresf.diazm@unac.edu.co>
2025-07-15 19:39:16 -0500

    routes

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const db = require('./models')
+const authRoutes = require('./routes/auth.routes')
 
 app.use(express.json())
+app.use('/api/auth',authRoutes);
 
 db.sequelize.sync({force:false}).then(()=>{
     console.log('Base de Datos Conectada');
